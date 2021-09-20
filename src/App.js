8752c3528a1021efb59694b780cee0c2c59a636d
kamilkulig <kamilmkulig@gmail.com>
2021-09-20 10:51:05 +0200

    `componentDidMount` refactored

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
 import React, { Component } from "react";
-import ReactDOM from "react-dom";
 import * as THREE from "three";
 
 class App extends Component {
   componentDidMount() {
-    // === THREE.JS CODE START ===
-    var scene = new THREE.Scene();
-    var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-    var renderer = new THREE.WebGLRenderer();
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    this.mount.appendChild( renderer.domElement );
-    var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-    var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-    var cube = new THREE.Mesh( geometry, material );
-    scene.add( cube );
-    camera.position.z = 5;
-    var animate = function () {
+    this.scene = new THREE.Scene();
+    this.camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+    this.renderer = new THREE.WebGLRenderer({
+      antialias: true, // disable for better performance
+      powerPreference: "high-performance",
+    });
+    this.renderer.setSize( window.innerWidth, window.innerHeight );
+    this.mount.appendChild( this.renderer.domElement );
+    const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+    const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+    const cube = new THREE.Mesh( geometry, material );
+    this.scene.add( cube );
+    this.camera.position.z = 5;
+
+    const animate = () => {
       requestAnimationFrame( animate );
       cube.rotation.x += 0.01;
       cube.rotation.y += 0.01;
-      renderer.render( scene, camera );
+      this.renderer.render( this.scene, this.camera );
     };
     animate();
-    // === THREE.JS EXAMPLE CODE END ===
   }
   render() {
     return (
