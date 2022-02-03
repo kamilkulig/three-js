4e91787c60a36a4e63412e7fd5f2ddd7d6b493b3
Kamil <kamilmkulig@gmail.com>
2022-02-03 23:59:00 +0100

    Added logo text

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,18 +4,26 @@ import './style.module.css';
 function Header (props) {
 
   return <div style={{
-    fontFamily: `'Cutive Mono', monospace`,
+    fontFamily: `'Barlow', sans-serif`,
     fontSize: '2rem',
     color: '#fff',
     width: '100%',
     background: 'rgb(2,0,36)',
     background: 'linear-gradient(180deg, rgba(2,0,36,1) 68%, rgba(0,0,0,0) 100%)',
-    height: '7rem'
+    height: '7rem',
   }}>
-    <img style={{
-      width: '5rem',
-    }} src='resources/logo/sun.jpg'  />
+    <div style={{
+      display: 'flex',
+      alignItems: 'center',
+      padding: '1rem'}}
+    >
+      <img style={{
+        width: '5rem',
+        height: '5rem',
+      }} src='resources/logo/sun.jpg'  />
+      <span>Kamil </span><span style={{fontWeight: 100}}>&nbsp;Kulig</span>
     
+    </div>
   </div>
 } 
 
