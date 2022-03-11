a18b1a580c769de3d9fc8fbec196f14ae27118c4
Kamil Kulig <kamilmkulig@gmail.com>
2022-03-11 21:24:34 +0100

    Removed old content

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import Environment3d from "./components/Environment3d/Environment3d";
-import Header from './components/Header'
 import './App.css';
 
 class App extends Component {
@@ -9,16 +8,9 @@ class App extends Component {
   }
   render() {
     return (
-      <div style={{
-        // 'background-image': 'url(resources/mock/3d.jpg)',
-        // 'background-size': 'contain',
-        // 'background-repeat': 'no-repeat',
-        // width: '100vw',
-        // height: '100vh',
-      }}>
+
         <div className='threeContainer' ref={ref => (this.mount = ref)} />
-        <Header />
-      </div>
+
     )
   }
   
