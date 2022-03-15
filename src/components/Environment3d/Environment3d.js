e126027f481239fe0672f7e85389d83d1c6149d5
Kamil Kulig <kamilmkulig@gmail.com>
2022-03-15 20:40:59 +0100

    Added simple cube

--- src/components/Environment3d/Environment3d.js
+++ src/components/Environment3d/Environment3d.js
@@ -3,7 +3,7 @@ import * as THREE from 'three';
 import Stats from 'three/examples/jsm/libs/stats.module.js';
 
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader.js';
+// import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader.js';
 import { Colors } from '../../utils/colors';
 
 class Environment3d {
@@ -26,69 +26,30 @@ class Environment3d {
       const container = document.createElement( 'div' );
       document.body.appendChild( container );
 
-      camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 5000 );
+      camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+      camera.position.z = 2;
+      camera.position.y = 5;
+      camera.position.x = 5;
       window.camera = camera; // for debugging purposes
-      camera.position.set(
-        -229.7125902247501,
-        114.1736637719915,
-        80.44154890605155
-      );
-      
-      console.log(camera);
+
 
       scene = new THREE.Scene();
-      scene.background = new THREE.Color( Colors.BLACK );
-      scene.fog = new THREE.Fog( Colors.BLACK, 200, 1000 );
+      scene.background = new THREE.Color( Colors.WHITE );
+      //scene.fog = new THREE.Fog( Colors.BLACK, 200, 1000 );
 
       const hemiLight = new THREE.HemisphereLight( 0xffffff, 0x444444 );
       hemiLight.position.set( 0, 200, 0 );
       scene.add( hemiLight );
 
-      const dirLight = new THREE.DirectionalLight( 0xffffff );
-      dirLight.position.set( 0, 200, 100 );
-      dirLight.castShadow = true;
-      dirLight.shadow.camera.top = 180;
-      dirLight.shadow.camera.bottom = - 100;
-      dirLight.shadow.camera.left = - 120;
-      dirLight.shadow.camera.right = 120;
-      scene.add( dirLight );
+      // const dirLight = new THREE.DirectionalLight( 0xffffff );
+      // dirLight.position.set( 0, 200, 100 );
+      // dirLight.castShadow = true;
+      // dirLight.shadow.camera.top = 180;
+      // dirLight.shadow.camera.bottom = - 100;
+      // dirLight.shadow.camera.left = - 120;
+      // dirLight.shadow.camera.right = 120;
+      // scene.add( dirLight );
 
-      // scene.add( new THREE.CameraHelper( dirLight.shadow.camera ) );
-
-      // ground
-      const mesh = new THREE.Mesh( new THREE.PlaneGeometry( 2000, 2000 ), new THREE.MeshPhongMaterial( { color: 0x999999, depthWrite: false } ) );
-      mesh.rotation.x = - Math.PI / 2;
-      mesh.receiveShadow = true;
-      scene.add( mesh );
-
-      const grid = new THREE.GridHelper( 2000, 20, 0x000000, 0x000000 );
-      grid.material.opacity = 0.2;
-      grid.material.transparent = true;
-      scene.add( grid );
-
-      // model
-      const loader = new FBXLoader();
-      loader.load( 'resources/terrain/Terrain_1.fbx', function ( object ) {
-
-        // mixer = new THREE.AnimationMixer( object );
-
-        // const action = mixer.clipAction( object.animations[ 0 ] );
-        // action.play();
-
-        object.traverse( function ( child ) {
-
-          if ( child.isMesh ) {
-
-            child.castShadow = true;
-            child.receiveShadow = true;
-
-          }
-
-        } );
-
-        scene.add( object );
-
-      } );
 
       renderer = new THREE.WebGLRenderer( { antialias: true } );
       renderer.setPixelRatio( window.devicePixelRatio );
@@ -97,7 +58,7 @@ class Environment3d {
       container.appendChild( renderer.domElement );
 
       const controls = new OrbitControls( camera, renderer.domElement );
-      controls.target.set( 0, 100, 0 );
+      controls.target.set( 0, 0, 0 );
       controls.update();
 
       window.addEventListener( 'resize', onWindowResize );
@@ -108,6 +69,47 @@ class Environment3d {
       stats.domElement.style.left = 'calc(100% - 80px)';
       stats.domElement.style.bottom = '0';
       container.appendChild( stats.dom );
+
+
+
+
+      const objects = [];
+      const spread = 15;
+       
+      function addObject(x, y, obj) {
+        obj.position.x = x * spread;
+        obj.position.y = y * spread;
+       
+        scene.add(obj);
+        objects.push(obj);
+      }
+
+
+      function createMaterial() {
+        const material = new THREE.MeshPhongMaterial({
+          side: THREE.DoubleSide,
+        });
+       
+        const hue = Math.random();
+        const saturation = 1;
+        const luminance = .5;
+        material.color.setHSL(hue, saturation, luminance);
+       
+        return material;
+      }
+
+      function addSolidGeometry(x, y, geometry) {
+        const mesh = new THREE.Mesh(geometry, createMaterial());
+        addObject(x, y, mesh);
+      }
+
+
+      const width = 8;
+      const height = 8;
+      const depth = 8;
+      addSolidGeometry(-2, -2, new THREE.BoxGeometry(width, height, depth));
+
+
 
     }
 
--- src/utils/colors.js
+++ src/utils/colors.js
@@ -1,4 +1,5 @@
 export const Colors = {
     NAVY: 'rgba(2,0,36,1)',
     BLACK: 'rgba(0,0,0,1)',
+    WHITE: 'rgba(255,255,255,1)'
 }
